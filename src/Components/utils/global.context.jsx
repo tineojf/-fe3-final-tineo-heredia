b21c7ea82c1be0536fec482f1cb48ceecbcabb8b
tineojf <92743762+tineodev@users.noreply.github.com>
2024-09-11 10:03:25 -0500

    Move usEffect api to context

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,5 +1,6 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { initialState, reducer } from "./reduceFile";
+import axios from "axios";
 
 export const ContextGlobal = createContext(undefined);
 
@@ -7,6 +8,18 @@ export const ContextProvider = (propiedades) => {
   const { children } = propiedades;
 
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const url = "https://jsonplaceholder.typicode.com/users";
+  useEffect(() => {
+    const fetchAPI = async (url) => {
+      const res = await axios.get(url);
+      const data = await res.data;
+      // console.log(data);
+      dispatch({ type: "LOAD-DATA", payload: data });
+    };
+
+    fetchAPI(url);
+  }, [dispatch]);
 
   return (
     <ContextGlobal.Provider value={{ state, dispatch }}>
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,27 +1,14 @@
-import { useEffect } from "react";
-import axios from "axios";
 import { useContextGlobal } from "../Components/utils/useContextFunction";
 import Card from "../Components/Card";
 
 const Home = () => {
-  const { state, dispatch } = useContextGlobal();
-  const url = "https://jsonplaceholder.typicode.com/users";
-
-  useEffect(() => {
-    const fetchAPI = async (url) => {
-      const res = await axios.get(url);
-      const data = await res.data;
-      // console.log(data);
-      dispatch({ type: "LOAD-DATA", payload: data });
-    };
-
-    fetchAPI(url);
-  }, [dispatch]);
+  const { state } = useContextGlobal();
 
   const dataHTML =
     state.data.length == 0
       ? "Cargando..."
       : state.data.map((item) => <Card key={item.id} item={item} />);
+
   return (
     <main className="">
       <h1 className="py-4">Home</h1>
